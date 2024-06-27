623a685a7c94f129f644d35516190c65f249bd4a
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-06-27 16:45:52 +0900

    학습내용추가

--- src/lang/immutable/change/ImmutableMain2.java
+++ src/lang/immutable/change/ImmutableMain2.java
@@ -4,7 +4,6 @@ public class ImmutableMain2 {
     public static void main(String[] args) {
         ImmutableObj obj1 = new ImmutableObj(10);
         obj1.add(20);
-        obj1.add(20);
 
         System.out.println(obj1);
     }
