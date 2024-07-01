02599993e303661d61c4cce6662bc18defe2fbec
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-07-01 22:13:26 +0900

    StringBuilder

---
+++ src/lang/string/builder/StringBuilderMain1_1.java
@@ -0,0 +1,25 @@
+package lang.string.builder;
+
+public class StringBuilderMain1_1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("A");
+        sb.append("B");
+        sb.append("C");
+        sb.append("D");
+        System.out.println("append = " + sb);
+
+        sb.insert(4,"Java");
+        System.out.println("insert : " + sb);
+
+        sb.delete(0,4);
+        System.out.println("delete : " + sb);
+
+        sb.reverse();
+        System.out.println("reverse : " + sb);
+
+        //StringBuilder -> String
+        String string = sb.toString();
+        System.out.println("string = " + string);
+    }
+}
